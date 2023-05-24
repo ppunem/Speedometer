d04a4b7592f3bd5d45c7ea3b180704e1ebfae912
ppunem <prathyusha2094@gmail.com>
2023-05-24 12:33:20 +0000

    second

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,28 +1,39 @@
 // Write your code here
-import Component from 'react'
+import {Component} from 'react'
 import './index.css'
 
-class Speedometer extends Component{
-    state={count=0}
+class Speedometer extends Component {
+  state = {count: 0}
+  increase = () => {
+    this.setState(prevState => ({count: prevState.counter + 1}))
+  }
+  brake = () => {
+    this.setState(prevState => ({count: prevState.counter - 1}))
+  }
 
-    render(){
-        const {count}=this.state
-
-        render(){
-            return (
-                <div className="bg">
-                  <h1 className=main-heading>SPEEDOMETER</h1>
-                  <img src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png" alt="speedometer" className="heading"/>
-                <h1 className="heading">Speed is <span>{count}</span>mph</h1>
-                <p className="para">Min Limit is 0mph, Max Limit is 200mph</p>
-                <div>
-                    <button className="btn1" type="button">Accelerate</button>
-                    <button className="btn2" type="button">Apply Brake</button>
-                </div>
-                </div>
-            )
-        }
-    }
+  render() {
+    const {count} = this.state
+    return (
+      <div className="bg">
+        <h1 className="main-heading">SPEEDOMETER</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+          className="image"
+          alt="speedometer"
+        />
+        <h1 className="heading">`Speed is ${count}mph`</h1>
+        <p className="para">Min Limit is 0mph, Max Limit is 200mph</p>
+        <div className="buttons-container">
+          <button type="button" className="btn1" onClick={this.increase}>
+            Accelerate
+          </button>
+          <button type="button" className="btn2" onClick={this.brake}>
+            Apply Brake
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Speedometer
